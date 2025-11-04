1ffb89eabc3ea9a7ad33cfb6636378edd86001e3
r03iuL <robiulislam1806@gmail.com>
2025-11-04 12:46:45 +0600

    feat:Add mongodb

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 dotenv.config();
 
 
 const app = express();
+
+//middleware
 app.use(cors());
 app.use(express.json());
 
 
+
+
+const uri =process.env.MONGO_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get("/", (req, res) => {
-  res.send("🔥 Notes API is running...");
+  res.send("Notes API is running...");
 });
 
 
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
